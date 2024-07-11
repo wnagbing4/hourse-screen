fb35fa6d0bcdc4749be663f513c455ca2ee9d074
3396515303@qq.com <12267007+wangbing56@user.noreply.gitee.com>
2024-07-11 11:38:14 +0800

    全局引用echarts

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 将全局的echarts对象挂载到vue原型上
+// 别的组件中使用 this.$echarts 来使用echarts
+Vue.prototype.$echarts = window.echarts
 Vue.config.productionTip = false
 
 new Vue({
